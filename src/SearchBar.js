b37fd2b8c28bb5c0a4f815d4e37d24ea2b9ad492
Danna Bohnhoff <dannab101@gmail.com>
2023-09-26 20:18:25 -0400

    search as typing

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,53 +1,17 @@
 import { useState } from 'react'
 
-// function SearchBar(props) {
-//     let [searchTerm, setSearchTerm] = useState('')
-
-//     return (
-//         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
-//             <input type="text" placeholder="Enter a search term here" onChange={
-//                 (e) => setSearchTerm(e.target.value)
-//             } />
-
-//             <input type="submit" />
-
-//         </form>
-//     )
-// }
-
-// export default SearchBar
-
-// const SearchBar = ({ handleSearch, handleClear, term }) => {
-//     const handleChange = (e) => {
-//       handleSearch(e, e.target.value);
-//     };
-  
-//     return (
-//       <form>
-//         <input
-//           type="text"
-//           placeholder="Search for music..."
-//           value={term}
-//           onChange={handleChange}
-//         />
-//         <button type="submit">Search</button>
-//         <button type="button" onClick={handleClear}>
-//           Clear
-//         </button>
-//       </form>
-//     );
-//   };
-  
-//   export default SearchBar;
-
 const SearchBar = ({ handleSearch, handleClear }) => {
     const [term, setTerm] = useState('');
   
+    // const handleChange = (e) => {
+    //   setTerm(e.target.value);
+    // };
     const handleChange = (e) => {
-      setTerm(e.target.value);
+      const searchTerm = e.target.value;
+      setTerm(searchTerm);
+      handleSearch(e, searchTerm); // Trigger search as typing
     };
-  
+    
     const handleSubmit = (e) => {
       e.preventDefault();
       handleSearch(e, term);
